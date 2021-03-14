6670107f897a9d5a44baab0d5f01fe674033d7f3
Jaivardhan Singh <jaisingh2908@gmail.com>
2021-03-14 23:37:51 +0530

    changes to use https v1.0.1

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
-const apiUrl = "http://localhost:4001/";
-// const apiUrl = "http://192.168.0.110:4001/";
-const socketUrl = "ws://localhost:4001/";
-// const socketUrl = "ws://192.168.0.110:4001/";
+// const apiUrl = "http://localhost:4001/";
+const apiUrl = "https://api-chat.jaivardhansingh.tech/";
+// const socketUrl = "ws://localhost:4001/";
+const socketUrl = "wss://api-chat.jaivardhansingh.tech/";
 export { apiUrl, socketUrl };
